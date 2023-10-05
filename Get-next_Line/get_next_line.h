8dba6aba826890417ae0feed2466454ff6c3b9f0
Iportill2 <146456723+Iportill2@users.noreply.github.com>
2023-10-05 13:02:11 +0200

    Add files via upload
    get next line

---
+++ Get-next_Line/get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iportill <iportill@student.42urduliz.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/06 16:24:31 by iportill          #+#    #+#             */
+/*   Updated: 2023/03/06 16:25:51 by iportill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <fcntl.h>/* para ..*/
+# include <unistd.h>/* para write*/
+# include <stdlib.h>/* para malloc*/
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_read_to_left_str(int fd, char *left_str);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *left_str, char *buff);
+size_t	ft_strlen(char *s);
+char	*ft_get_line(char *left_str);
+char	*ft_new_left_str(char *left_str);
+
+#endif
