6843578c5864c882e536bdbf82517c8e3e85e6d3
Iportill2 <146456723+Iportill2@users.noreply.github.com>
2023-10-05 13:03:14 +0200

    Add files via upload
    Libft

---
+++ Libft/ft_substr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iportill <iportill@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/07 17:30:39 by iportill          #+#    #+#             */
+/*   Updated: 2022/12/12 15:20:37 by iportill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*dest;
+
+	if (!s)
+		return (NULL);
+	if (start > ft_strlen(s))
+		return (ft_strdup(""));
+	if (ft_strlen(s) < len)
+		len = ft_strlen(s);
+	dest = (char *)malloc(sizeof(char) * len + 1);
+	if (!dest)
+		return (NULL);
+	ft_strlcpy(dest, &s[start], len + 1);
+	return (dest);
+}
